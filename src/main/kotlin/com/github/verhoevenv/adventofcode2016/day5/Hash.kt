8c9de2afa8b2d07d9bada413a609ecb2f9d13a12
Vincent Verhoeven <verhoevenv@gmail.com>
2016-12-10 01:50:16 +0100

    Day 5 part 2

--- src/main/kotlin/com/github/verhoevenv/adventofcode2016/day5/Hash.kt
+++ src/main/kotlin/com/github/verhoevenv/adventofcode2016/day5/Hash.kt
@@ -4,30 +4,51 @@ package com.github.verhoevenv.adventofcode2016.day5
 val md5 = java.security.MessageDigest.getInstance("MD5")
 
 fun password(doorId: String): String {
-    val numbers = generateSequence(1) { i -> i + 1 }
-    return numbers.map { n -> doorId + n }.map(::passwordChar).filter { x -> x != null }.take(8).joinToString(separator = "")
+    return hashes(doorId).filter(Hash::startsWith5Zeroes).map(Hash::sixthChar).take(8).joinToString(separator = "")
 }
 
-fun passwordChar(s: String): Char? = extractPasswordChar(hash(s))
+private fun hashes(doorId: String): Sequence<Hash> {
+    return generateSequence(1) { i -> i + 1 }.map { n -> doorId + n }.map(::hash)
+}
 
-fun extractPasswordChar(ba: ByteArray): Char? {
-    val zero: Byte = 0
-    if(ba[0] == zero && ba[1] == zero) {
-        return toAsciiChar(ba[2])
+fun orderedPassword(doorId: String): String {
+    return hashes(doorId)
+            .filter(Hash::startsWith5Zeroes)
+            .filter { x -> x.sixthChar() in '0'..'7' }
+            .distinctBy(Hash::sixthChar)
+            .take(8)
+            .sortedBy(Hash::sixthChar)
+            .map(Hash::seventhChar)
+            .joinToString(separator = "")
+}
+
+data class Hash(val ba: ByteArray) {
+    fun startsWith5Zeroes(): Boolean {
+        val zero: Byte = 0
+        return ba[0] == zero && ba[1] == zero && (ba[2].toInt() and 0xF0) == 0
     }
-    return null
+
+    fun sixthChar(): Char {
+        return toAsciiChar((ba[2].toInt() and 0x0F).toByte())
+    }
+
+    fun seventhChar(): Char {
+        return toAsciiChar((ba[3].toInt() and 0xF0).shr(4).toByte())
+    }
+
 }
 
-fun toAsciiChar(byte: Byte): Char? {
+
+fun toAsciiChar(byte: Byte): Char {
     return when(byte) {
         in(0..9) -> '0' + byte.toInt()
         in(10..15) -> 'a' + byte.toInt() - 10
-        else -> null
+        else -> throw IllegalArgumentException("argument doesn't fit in hexadec char: $byte")
     }
 }
 
-fun hash(s: String): ByteArray {
+fun hash(s: String): Hash {
     val result = md5.digest(s.toByteArray())
     md5.reset()
-    return result
+    return Hash(result)
 }
--- src/test/kotlin/com/github/verhoevenv/adventofcode2016/day5/HashSpec.kt
+++ src/test/kotlin/com/github/verhoevenv/adventofcode2016/day5/HashSpec.kt
@@ -7,21 +7,29 @@ class HashSpec: StringSpec() {
         "password" {
             password("abc") shouldBe "18f47a30"
         }
-
-        "passwordChar" {
-            passwordChar("abc3231928") shouldBe null
-            passwordChar("abc3231929") shouldBe '1'
+        "orderedPassword" {
+            orderedPassword("abc") shouldBe "05ace8e3"
         }
 
-        "extractPasswordChar" {
-            extractPasswordChar(bytes(0x00, 0x00, 0x01)) shouldBe '1'
-            extractPasswordChar(bytes(0x00, 0x00, 0x0A)) shouldBe 'a'
-            extractPasswordChar(bytes(0x00, 0x00, 0x0F)) shouldBe 'f'
-            extractPasswordChar(bytes(0x10, 0x00, 0x0F)) shouldBe null
-            extractPasswordChar(bytes(0x00, 0x00, 0x10)) shouldBe null
+        "sixthChar" {
+            Hash(bytes(0x00, 0x00, 0x01)).sixthChar() shouldBe '1'
+            Hash(bytes(0x00, 0x00, 0x0A)).sixthChar() shouldBe 'a'
+            Hash(bytes(0x00, 0x00, 0x0F)).sixthChar() shouldBe 'f'
+        }
+
+        "seventhChar" {
+            Hash(bytes(0x00, 0x00, 0x00, 0x10)).seventhChar() shouldBe '1'
+            Hash(bytes(0x00, 0x00, 0x00, 0xA0)).seventhChar() shouldBe 'a'
+            Hash(bytes(0x00, 0x00, 0x00, 0xF0)).seventhChar() shouldBe 'f'
+        }
+
+        "isUsableHash" {
+            Hash(bytes(0x00, 0x00, 0x01)).startsWith5Zeroes() shouldBe true
+            Hash(bytes(0x10, 0x00, 0x0F)).startsWith5Zeroes() shouldBe false
+            Hash(bytes(0x00, 0x00, 0x10)).startsWith5Zeroes() shouldBe false
         }
 
     }
 
-    private fun bytes(vararg bs: Byte): ByteArray = bs
+    private fun bytes(vararg bs: Int): ByteArray = bs.map(Int::toByte).toByteArray()
 }
