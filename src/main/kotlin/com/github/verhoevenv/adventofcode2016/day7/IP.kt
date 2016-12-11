276f8b2afee02579eb4c514d10f088dc86e4a535
Vincent Verhoeven <verhoevenv@gmail.com>
2016-12-11 23:16:15 +0100

    Day 7 part 2

--- src/main/kotlin/com/github/verhoevenv/adventofcode2016/day7/IP.kt
+++ src/main/kotlin/com/github/verhoevenv/adventofcode2016/day7/IP.kt
@@ -4,21 +4,47 @@ fun String.startsWithABBA(): Boolean {
     return this.length >= 4 && this[0] == this[3] && this[1] == this[2] && this[0] != this[1]
 }
 
+fun String.startsWithABA(): Boolean {
+    return this.length >= 3 && this[0] == this[2] && this[0] != this[1]
+}
+
 fun String.containsABBA(): Boolean {
     return (0..this.length).any { this.substring(it).startsWithABBA() }
 }
 
 data class IPv7(val parts: Set<AddressPart>) {
+
+    private fun hypernetParts(): List<String> = parts.filterIsInstance<HyperNetAddress>().map { it.adress }
+    private fun normalParts(): List<String> = parts.filterIsInstance<NormalAdress>().map { it.adress }
+
     fun supportsTLS(): Boolean {
-        return parts.filterIsInstance<NormalAdress>().any { it.adress.containsABBA() } &&
-                parts.filterIsInstance<HyperNetAddress>().none() { it.adress.containsABBA() }
+        return normalParts().any { it.containsABBA() } &&
+                hypernetParts().none() { it.containsABBA() }
 
     }
+
+    fun supportsSSL(): Boolean {
+        val abas = normalParts().flatMap { it.getABAs() }
+        return abas.map(ABA::bab).any { bab -> hypernetParts().any{ part -> part.contains(bab) } }
+    }
+
 }
 
 interface AddressPart
 data class HyperNetAddress(val adress: String) : AddressPart
 data class NormalAdress(val adress: String) : AddressPart
+
+fun String.getABAs(): List<ABA> {
+    return (0..this.length).map { this.substring(it) }.filter(String::startsWithABA).map { ABA(it[0], it[1]) }
+}
+
+
+data class ABA(val a: Char, val b: Char) {
+    constructor(aba: String) : this(aba[0], aba[1]) {
+        require(aba.length == 3 && aba.startsWithABA())
+    }
+    val bab: String = "$b$a$b"
+}
 
 fun parse(s: String): IPv7 {
     val hypernetRegex = """\[([^\]]*)\]""".toRegex()
--- src/test/kotlin/com/github/verhoevenv/adventofcode2016/day7/IPSpec.kt
+++ src/test/kotlin/com/github/verhoevenv/adventofcode2016/day7/IPSpec.kt
@@ -25,6 +25,20 @@ class IPSpec  : StringSpec() {
             IPv7(setOf(NormalAdress("ioxxoj"), HyperNetAddress("asdfgh"), NormalAdress("zxcvbn"))).supportsTLS() shouldBe true
         }
 
+        "supportsSSL" {
+            IPv7(setOf(NormalAdress("aba"), HyperNetAddress("bab"), NormalAdress("xyz"))).supportsSSL() shouldBe true
+            IPv7(setOf(NormalAdress("xyx"), HyperNetAddress("xyx"), NormalAdress("xyx"))).supportsSSL() shouldBe false
+            IPv7(setOf(NormalAdress("aaa"), HyperNetAddress("kek"), NormalAdress("eke"))).supportsSSL() shouldBe true
+            IPv7(setOf(NormalAdress("zazbz"), HyperNetAddress("bzb"), NormalAdress("cdb"))).supportsSSL() shouldBe true
+            IPv7(setOf(NormalAdress("zazbz"), HyperNetAddress("argbzbirg"), NormalAdress("cdb"))).supportsSSL() shouldBe true
+        }
+
+        "aba's" {
+            "aba".getABAs() shouldBe listOf(ABA("aba"))
+            "aaa".getABAs() shouldBe emptyList<String>()
+            "zazbz".getABAs() shouldBe listOf(ABA("zaz"), ABA("zbz"))
+        }
+
         "parse" {
             parse("abba[mnop]qrst") shouldBe IPv7(setOf(NormalAdress("abba"), NormalAdress("qrst"), HyperNetAddress("mnop")))
         }
@@ -32,7 +46,7 @@ class IPSpec  : StringSpec() {
         "input examples" {
             parse("spwwppgjgfexuezrixp[rotgzyxzqxyrroafx]tkwxfiamzdjdqpftvq").supportsTLS() shouldBe true
         }
-        
+
     }
 }
 
